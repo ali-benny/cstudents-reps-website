b4603e5708a048a9f754c506b676547b47d87f36
Alice Benatti <alice17bee@gmail.com>
2025-11-25 10:49:09 +0100

    add: vite preview

--- vite.config.ts
+++ vite.config.ts
@@ -7,17 +7,19 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [tailwindcss(), vue(), vueDevTools()],
   build: {
-    assetsDir: 'static'
+    outDir: 'dist',
+    assetsDir: 'static',
+  },
+  preview: {
+    port: 3000,
+    host: true,
+    allowedHosts: ['reps.students.cs.unibo.it']
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
