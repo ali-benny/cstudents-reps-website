74dbe111146ccef6827c319e5818817bef3334fa
Alice Benatti <alice17bee@gmail.com>
2026-09-10 17:12:57 +0200

    chore: fix aula

--- src/components/EventsHomeTitle.vue
+++ src/components/EventsHomeTitle.vue
@@ -1,12 +1,8 @@
 <template>
   <div class="relative w-full mb-8">
     <div
-      class="absolute inset-0 bg-gradient-to-r from-orange-500 via-pink-500 to-red-500 rounded-3xl blur-xl opacity-60 animate-pulse"
-    ></div>
-    <div
-      class="relative bg-gradient-to-br from-secondary to-accent rounded-3xl px-4 py-6 md:px-12 md:py-8 border-2 border-white/30"
-    >
-      <div class="absolute -top-4 -right-6 md:-right-8 z-10">
+      class="relative bg-base-100/25 rounded-3xl px-4 py-6 md:px-12 md:py-8 border-2 border-white/30">
+      <!-- <div class="absolute -top-4 -right-6 md:-right-8 z-10">
         <div
           class="inline-flex items-center gap-2 px-4 py-2 md:px-6 md:py-3 bg-gradient-to-r from-orange-500 to-pink-500 rounded-full shadow-lg transform rotate-12"
         >
@@ -14,37 +10,50 @@
           <span class="font-bold text-sm md:text-lg">ONLINE</span>
           <Icon icon="heroicons:sparkles" class="h-5 w-5 md:h-6 md:w-6" />
         </div>
+      </div> -->
+
+      <div class="relative w-full mb-6">
+        <!-- <h1 class="text-3xl md:text-6xl lg:text-7xl leading-tight">Welcome Days</h1> -->
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-2">
+          <div class="flex items-between flex-col h-full border-b-2 pb-2 md:pb-0 md:border-r-2 md:border-b-0">
+            <h2 class="text-3xl md:text-6xl">Welcome Day</h2>
+
+            <h5 class="text-lg md:text-2xl font-light italic"> Matricole a.a. 2026/2027</h5>
+            <h5 class="text-md md:text-lg text-pretty">Due chiacchiere con noi per chiarire ogni dubbio<br>e
+              presentazione dei gruppi studenteschi del dipartimento<br>(ADMStaff, Cartabinaria, ULISSE)</h5>
+            <div class="font-bold text-md md:text-xl lg:text-3xl mt-4 leading-tight bg-secondary/80 rounded-xl mx-4 my-auto">
+              martedì 22 settembre 2026
+              <h5 class="flex gap-1 justify-center items-baseline flex-wrap">
+                <p class="md:text-2xl">dalle 18:00 in Aula Pincherle,</p>
+                <p class="md:text-xl font-light">Dip. Matematica</p>
+              </h5>
+            </div>
+          </div>
+          <div>
+            <h2 class="text-3xl md:text-6xl">How to 3 anno</h2>
+            <h5 class="text-lg md:text-2xl font-light italic">Dedicato agli studenti del 3° anno Triennale!</h5>
+            <h5 class="text-md md:text-xl text-pretty">Insegnamenti a scelta, piano didattico, tirocinio,
+              tesi,<br>esami
+              e tutto quello che c'è da sapere per affrontare al meglio il 3° anno.
+            </h5>
+            <div class="font-bold text-md md:text-xl lg:text-3xl mt-4 leading-tight bg-primary/80 rounded-xl mx-4 mt-auto">
+              giovedì 24 settembre 2026
+              <h5 class="flex gap-1 justify-center items-baseline flex-wrap">
+                <p class="md:text-2xl">dalle 11:00 in Aula G,</p>
+                <p class="md:text-xl font-light text-wrap flex flex-wrap">Dip. Giurisprudenza in via Andreatta 8</p>
+              </h5>
+            </div>
+          </div>
+        </div>
       </div>
-
-      <div class="relative w-full mb-6 border-b-4 border-base-content/50">
-        <h1 class="text-3xl md:text-6xl lg:text-7xl leading-tight">Assemblea Studentesca</h1>
-        <h2 class="text-xl md:text-4xl">Proposte per i nuovi Piani Didattici a.a. 2027-2028</h2>
-
-        <h5 class="my-5 text-lg md:text-2xl font-light italic">
-          Un momento di ascolto e confronto per cambiare le cose!
-        </h5>
-        <h5 class="text-md md:text-lg text-pretty">
-          Le opinioni raccolte saranno poi raggruppate per approvare o meno le proposte,<br />in
-          modo da dare il parere degli studenti ai docenti della Commissione Piani Didattici.
-        </h5>
-      </div>
-      <h3 class="text-2xl md:text-xl lg:text-3xl mb-4 leading-tight">
-        mercoledì 20 maggio 2026 | dalle 16:00
-      </h3>
-
-      <div
-        class="flex flex-col lg:flex-row gap-4 justify-center items-center w-full *:text-md md:*:text-lg"
-      >
-        <a
-          href="https://teams.microsoft.com/meet/386438130758200?p=AivLAvAF5qWFYUM3g6"
-          target="_blank"
-          class="btn gap-2 font-semibold min-w-0 w-full lg:w-auto h-auto min-h-0 flex items-center justify-center px-4 py-3 bg-white text-secondary border-none shadow-lg"
-        >
+      <div class="flex flex-col lg:flex-row gap-4 justify-center items-center w-full *:text-md md:*:text-lg">
+        <a href="https://teams.microsoft.com/meet/315942013653168?p=Di5VAhx1L0LIelI3Cv" target="_blank"
+          class="btn gap-2 font-semibold min-w-0 w-full lg:w-auto h-auto min-h-0 flex items-center justify-center px-4 py-3 bg-white text-secondary border-none shadow-lg">
           <Icon icon="logos:microsoft-teams" class="h-6 w-6 min-w-fit" />
-          <span class="whitespace-normal break-words text-center">Entra in chiamata su Teams</span>
+          <span class="whitespace-normal break-words text-center">Segui gli eventi anche su Teams</span>
         </a>
 
-        <a
+        <!-- <a
           href="https://forms.office.com/e/mn2W1wKw4a"
           target="_blank"
           class="btn gap-2 font-semibold min-w-0 w-full lg:w-auto h-auto min-h-0 flex items-center justify-center px-4 py-3 border-none shadow-lg hover:scale-105 transition-all duration-200"
@@ -53,7 +62,7 @@
           <span class="whitespace-normal break-words text-center"
             >Dai la tua opinione tramite il Sondaggio Online</span
           >
-        </a>
+        </a> -->
       </div>
     </div>
   </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@ import { ref, onMounted, onUnmounted } from 'vue'
 import ComunicationSection from '@/components/ComunicationSection.vue'
 import ActivitiesSection from '@/components/ActivitiesSection.vue'
 import { scrollToSection, isProgrammaticScroll } from '@/utils'
-// import EventsHomeTitle from '@/components/EventsHomeTitle.vue'
+import EventsHomeTitle from '@/components/EventsHomeTitle.vue'
 
 interface Representative {
   id: number | string
@@ -21,7 +21,7 @@ const representatives = ref<Representative[]>([
     name: 'Alice Benatti',
     email: 'alice.benatti4@studio.unibo.it',
     telegram: '@alii_benatti',
-    year: '1°',
+    year: '2°',
     curriculum: 'Magistrale',
   },
   {
@@ -29,7 +29,7 @@ const representatives = ref<Representative[]>([
     name: 'William Brusa',
     email: 'william.brusa@studio.unibo.it',
     telegram: '@OkGuh',
-    year: '2°',
+    year: '3°',
     curriculum: 'Triennale',
   },
   {
@@ -37,7 +37,7 @@ const representatives = ref<Representative[]>([
     name: 'Rohan Regmi',
     email: 'rohan.regmi@studio.unibo.it',
     telegram: '@dark24x7',
-    year: '2°',
+    year: '3°',
     curriculum: 'Triennale',
   },
 ])
@@ -97,8 +97,8 @@ onUnmounted(() => {
           class="h-24 md:h-32 mx-auto drop-shadow-lg"
         />
       </div>
-      <!-- <EventsHomeTitle></EventsHomeTitle> -->
-      <h1 class="text-3xl md:text-6xl lg:text-7xl leading-tight">Rappresentanti Studenti</h1>
+      <EventsHomeTitle></EventsHomeTitle>
+      <!-- <h1 class="text-3xl md:text-6xl lg:text-7xl leading-tight">Rappresentanti Studenti</h1>
       <h3 class="text-2xl md:text-4xl lg:text-5xl mb-6 leading-tight">Informatica L e LM</h3>
       <h3 class="text-lg md:text-xl mb-6 leading-tight">
         Alma Mater Studiorum - Università di Bologna
@@ -107,7 +107,7 @@ onUnmounted(() => {
       <p class="text-xl md:text-2xl mb-8 /90 max-w-3xl mx-auto leading-relaxed">
         La tua voce nel Corso di Laurea: news, bandi, assemblee, opportunità e supporto per
         migliorare insieme il tuo percorso accademico.
-      </p>
+      </p> -->
 
       <!-- CTA Buttons -->
       <div class="flex flex-col sm:flex-row gap-4 justify-center items-center">
